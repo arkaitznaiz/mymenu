4ccc6912cad85dd33aef872cde25d7a0a9e414cc
Arkaitz Mugica Islas <arkaitzmugica@protonmail.com>
2020-06-29 07:04:54 +0200

    Improved some visual aspects

--- mymenu/settings.py
+++ mymenu/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5rujiak$u7gp$@d!bv-k9pr6+17*=ok)-cz^i-%m=fb!3crm^j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if DEBUG:
     ALLOWED_HOSTS = []
